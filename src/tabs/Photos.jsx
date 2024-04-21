04d7ec52588047785a6da92505bc7187d1d654af
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-21 15:25:18 +0300

    add form

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,13 @@
 // import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { Text, Form } from 'components';
 
 export const Photos = () => {
+  const onSubmit = text => {
+    console.log(text);
+  };
   return (
     <>
+      <Form onSubmit={onSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
